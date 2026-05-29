74cfaa467749fac47b36027804b26ed0237837fa
Adarsh Sharma Nadda <adarshauspicious@gmail.com>
2026-05-29 13:23:13 +0530

    Changes

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res) => {
         id: user._id,
       },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" },
+      { expiresIn: "1d" }, 
     );
     res.cookie("token",token,{
         httpOnly: true,
